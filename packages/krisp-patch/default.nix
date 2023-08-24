ef9d3e13a8b8ce93f88ccffe410cc034d6c0d7bd
nat <nat@nekopon.pl>
2023-08-25 00:59:08 +0200

    krisp-patch: fixup

--- packages/krisp-patch/default.nix
+++ packages/krisp-patch/default.nix
@@ -11,8 +11,7 @@ writeShellApplication rec {
   text = ''
     discord_version="${discord-canary.version}"
     file="$HOME/.config/discordcanary/$discord_version/modules/discord_krisp/discord_krisp.node"
-
-    addr=$(rz-find -x '4889dfe8........4889dfe8' "$file" | head -n1)
-    rizin -q -w -c "s $addr + 0x12 ; wao nop" "$file"
+    addr=$(rz-find -x '4881ec00010000' "$file" | head -n1)
+    rizin -q -w -c "s $addr + 0x30 ; wao nop" "$file"
   '';
 }
