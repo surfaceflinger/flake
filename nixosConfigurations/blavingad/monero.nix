5938e55aa5878360366c1c58be7ca124375dbbf0
nat <nat@nekopon.pl>
2023-08-27 21:23:02 +0200

    hosts/blavingad/monero: open p2p

--- nixosConfigurations/blavingad/monero.nix
+++ nixosConfigurations/blavingad/monero.nix
@@ -1,9 +1,12 @@
 _: {
+  networking.firewall.allowedTCPPorts = [ 18080 ];
+
   services.monero = {
     enable = true;
     rpc = {
       restricted = true;
       address = "0.0.0.0";
+      port = 18089;
     };
     limits = {
       download = 524288;
@@ -17,6 +20,10 @@ _: {
       in-peers=1024
       no-igd=true
       out-peers=64
+      p2p-bind-ip=0.0.0.0
+      p2p-bind-ipv6-address=::
+      p2p-bind-port=18080
+      p2p-use-ipv6=true
       prune-blockchain=true
       sync-pruned-blocks=true
     '';
