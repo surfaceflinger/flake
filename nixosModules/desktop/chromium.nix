232d0b4fb8ecc83dd8a80fa27063046983a63f1b
nat <nat@nekopon.pl>
2023-10-26 21:41:18 +0200

    nixosModules/desktop/chromium: install more bloat

--- nixosModules/desktop/chromium.nix
+++ nixosModules/desktop/chromium.nix
@@ -6,12 +6,14 @@
   programs.chromium = {
     enable = true;
     extensions = [
+      "ajhmfdgkijocedmfjonnpjfojldioehi" # Privacy Pass
       "blipmdconlkpinefehnmjammfjpmpbjk" # Lighthouse
       "cankofcoohmbhfpcemhmaaeennfbnmgp" # Netflix 1080p
       "chklaanhfefbnpoihckbnefhakgolnmc" # JSONVue
       "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
       "dneaehbmnbhcippjikoajpoabadpodje" # Old Reddit Redirect
       "dnhpnfgdlenaccegplpojghhmaamnnfp" # Augmented Steam
+      "donbcfbmhbcapadipfkeojnmajbakjdc" # ruffle.rs
       "edibdbjcniadpccecjdfdjjppcpchdlm" # I still don't care about cookies
       "emffkefkbkpkgpdeeooapgaicgmcbolj" # Wikiwand
       "enamippconapkdmgfgjchkhakpfinmaj" # DeArrow
@@ -20,19 +22,25 @@
       "hlepfoohegkhhmjieoechaddaejaokhf" # Refined GitHub
       "icallnadddjmdinamnolclfjanhfoafe" # FastForward
       "ipdchdaimepggclpjanobgdahjflhedd" # AWS Dark
+      "jinjaccalgkegednnccohejagnlnfdag" # Małpka
       "lckanjgmijmafbedllaakclkaicjfmnk" # ClearURLs
+      "ldpochfccmkkmhdbclfhpagapcfdljkj" # Decentraleyes
       "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock for YouTube
+      "mpbjkejclgfgadiemmefgebjfooflfhl" # Buster
       "ndpmhjnlfkgfalaieeneneenijondgag" # YouTube Anti Translate
       "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
       "ohnjgmpcibpbafdlkimncjhflgedgpam" # 4chan X
       "pampamgoihgcedonnphgehgondkhikel" # DotGit
+      "pkehgijcmpdhfbdbbnkijodmdjhbjlgp" # Privacy Badger
     ];
     extraOpts = {
       "AutofillAddressEnabled" = false;
       "AutofillCreditCardEnabled" = false;
       "BlockThirdPartyCookies" = true;
       "CloudPrintProxyEnabled" = false;
+      "ExtensionInstallBlocklist" = [ "*" ];
       "HistoryClustersVisible" = false;
+      "HttpsOnlyMode" = "force_enabled";
       "MetricsReportingEnabled" = false;
       "NetworkPredictionOptions" = 2;
       "PasswordManagerEnabled" = false;
@@ -45,6 +53,7 @@
       "SearchSuggestEnabled" = false;
       "SpellCheckServiceEnabled" = false;
       "SSLErrorOverrideAllowed" = true;
+      "UrlKeyedAnonymizedDataCollectionEnabled" = false;
     };
   };
 }
