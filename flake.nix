26723610c6f5c7a37af1380746bc51c38a3ba82c
nat <nat@nekopon.pl>
2023-11-11 19:35:15 +0100

    lock: update

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
   description = "nat's nix flake (:";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://cache.garnix.io"
-      "https://nix-community.cachix.org"
-      "https://numtide.cachix.org"
-      "https://nyx.chaotic.cx/"
-    ];
-    extra-trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-    ];
-  };
+#  nixConfig = {
+#    extra-substituters = [
+#      "https://cache.garnix.io"
+#      "https://nix-community.cachix.org"
+#      "https://numtide.cachix.org"
+#      "https://nyx.chaotic.cx/"
+#    ];
+#    extra-trusted-public-keys = [
+#      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+#      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+#      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+#      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+#      "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+#    ];
+#  };
 
   inputs = {
     agenix.inputs.nixpkgs.follows = "nixpkgs";
@@ -31,7 +31,7 @@
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
     nix-index-database.url = "github:Mic92/nix-index-database";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    nixpkgs.url = "github:surfaceflinger/nixpkgs/hardened-malloc-light";
+    nixpkgs.url = "github:surfaceflinger/nixpkgs/natpkgs";
     srvos.inputs.nixpkgs.follows = "nixpkgs";
     srvos.url = "github:nix-community/srvos";
     systems.url = "github:nix-systems/default-linux";
