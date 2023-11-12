0860afa31ba4d7833f1cfa45961713e0a61a2376
nat <nat@nekopon.pl>
2023-11-12 23:46:57 +0100

    nixosConfigurations/djungelskog: fixup

--- nixosConfigurations/djungelskog/default.nix
+++ nixosConfigurations/djungelskog/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
   imports = [
     inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
     inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t440p
@@ -23,6 +23,10 @@
 
   # Power management
   boot.kernelParams = [ "intel_pstate=active" ];
+  services.tlp.settings = {
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  };
   services.undervolt = {
     enable = true;
     coreOffset = -90;
