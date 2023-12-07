9f42b6b066c816ef7faeb3c0434d2bdae526b751
nat <nat@nekopon.pl>
2023-12-07 17:55:28 +0100

    nixosConfigurations/knorrig/storage: disable zram writeback
    finally got 16 gigs here lol

--- nixosConfigurations/knorrig/storage.nix
+++ nixosConfigurations/knorrig/storage.nix
@@ -5,36 +5,6 @@
 
   ephemereal = true;
 
-  zramSwap.writebackDevice = "/dev/zvol/knorrig/zramwriteback";
-
-  systemd.timers.zramwriteback-enable = {
-    description = "zram writeback timer to ensure writeback is always used";
-    partOf = [ "zramwriteback-enable.service" ];
-    wantedBy = [ "multi-user.target" ];
-    timerConfig = {
-      OnBootSec = "2min";
-      OnUnitActiveSec = "30";
-    };
-  };
-
-  systemd.services.zramwriteback-enable =
-    let
-      script = pkgs.writeScript "zramwriteback-enable" ''
-        #!${lib.getExe pkgs.bash}
-        echo all > /sys/block/zram0/idle
-        echo idle > /sys/block/zram0/writeback
-      '';
-    in
-    {
-      wantedBy = [ "multi-user.target" "post-resume.target" ];
-      after = [ "post-resume.target" "systemd-zram-setup@zram0.service" ];
-      serviceConfig = {
-        Type = "oneshot";
-        Restart = "no";
-        ExecStart = "${script}";
-      };
-    };
-
   fileSystems."/" = {
     device = "none";
     fsType = "tmpfs";
