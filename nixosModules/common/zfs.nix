e955628b96db128e1334c4eecd1bb56d3e965b2f
nat <nat@nekopon.pl>
2023-12-20 18:43:52 +0100

    nixosModules/common/zfs: tweak

--- nixosModules/common/zfs.nix
+++ nixosModules/common/zfs.nix
@@ -4,6 +4,11 @@
 , ...
 }:
 {
+  boot.kernelParams = [
+    "zfs.metaslab_lba_weighting_enabled=0"
+    "zfs.zfs_abd_scatter_enabled=0"
+  ];
+
   boot.postBootCommands = lib.optionalString (config.boot.zfs.enabled) ''
     # To debug check: journalctrl -b | grep stage-2-init
     echo Mounting all zfs filesystems
