5c3469ed63a67b63906cef0e69f38172fbe7c79a
nat <nat@nekopon.pl>
2024-01-18 20:49:05 +0100

    nixosModules/mixins/virtualisation: kvm_amd tweaks, lighter qemu

--- nixosModules/mixins/virtualisation.nix
+++ nixosModules/mixins/virtualisation.nix
@@ -7,16 +7,18 @@
     libvirtd = {
       enable = true;
       qemu = {
+        package = pkgs.qemu_kvm;
         runAsRoot = false;
-        ovmf.enable = true;
+        swtpm.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
       };
     };
   };
 
   boot.extraModprobeConfig = ''
-    options kvm_intel nested=1
-    options kvm_intel emulate_invalid_guest_state=0
-    options kvm ignore_msrs=1
+    options kvm_amd npt=1 avic=1 nested=1 sev=1
+    options kvm ignore_msrs=1 report_ignored_msrs=0
+    options kvm_intel nested=1 emulate_invalid_guest_state=0
   '';
 
   boot.kernelModules = [
