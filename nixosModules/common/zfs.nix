f9fda5851161a4848ab57372e25657dcab53a653
nat <nat@nekopon.pl>
2024-02-27 18:49:41 +0100

    nixosModules/common/zfs: explicitly enable trim and scrub and tweak intervals

--- nixosModules/common/zfs.nix
+++ nixosModules/common/zfs.nix
@@ -15,4 +15,15 @@
     echo Mounting all zfs filesystems
     ${pkgs.zfs}/bin/zfs mount -a
   '';
+
+  services.zfs = {
+    autoScrub = {
+      enable = true;
+      interval = "*-*-10 02:00:00 Europe/Warsaw";
+    };
+    trim = {
+      enable = true;
+      interval = "weekly";
+    };
+  };
 }
