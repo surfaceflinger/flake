206d9ba1f8681d433ad73d551d3fe682ac873ad2
nat <nat@nekopon.pl>
2024-03-09 15:43:35 +0100

    lock: update

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
     nix-index-database.url = "github:nix-community/nix-index-database/main";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
     nyx.inputs.nixpkgs.follows = "nixpkgs";
     nyx.url = "github:chaotic-cx/nyx/main";
     srvos.inputs.nixpkgs.follows = "nixpkgs";
