a996460b803996c3d8e53f279baaa1ee6811a115
nat <nat@nekopon.pl>
2024-04-21 13:33:48 +0200

    nixosModules/common/chrony: update NTS servers

--- nixosModules/common/chrony.nix
+++ nixosModules/common/chrony.nix
@@ -8,17 +8,34 @@ _: {
       dscp 46
       leapsectz right/UTC
       makestep 1.0 3
-      minsources 2
+      minsources 3
       nocerttimecheck 1
     '';
   };
   networking.timeServers = [
-    "time.cloudflare.com"
-    "virginia.time.system76.com"
+    # Sorted by TLDs to avoid
+    # using multiple servers
+    # from the same provider
+    "ntp.nanosrvr.cloud"
     "ptbtime1.ptb.de"
-    "ntppool1.time.nl"
-    "ntp.3eck.net"
+    "www.masters-of-cloud.de"
+    "nts.ntstime.de"
+    "ntp3.fau.de"
+    "ntp3.ipv6.fau.de"
+    "www.jabber-germany.de"
+    "time.bolha.one"
+    "gbg1.nts.netnod.se"
     "ntp.trifence.ch"
+    "time.signorini.ch"
+    "ntppool1.time.nl"
+    "paris.time.system76.com"
+    "time.cloudflare.com"
+    "time.txryan.com"
+    "a.st1.ntp.br"
+    "ntp.3eck.net"
     "ntp.zeitgitter.net"
+    "ntp.miuku.net"
+    "ntpmon.dcs1.biz"
+    "stratum1.time.cifelli.xyz"
   ];
 }
