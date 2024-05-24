ca2adafaaa6283e310bc51549d1c06569203cd10
nat <nat@nekopon.pl>
2024-05-24 10:45:22 +0200

    nixosModules/common/nix: switch to upstream lix

--- nixosModules/common/nix.nix
+++ nixosModules/common/nix.nix
@@ -2,6 +2,7 @@
 {
   # Nix/nixpkgs                                                                                                                                                                                                                                                                          
   imports = [
+    inputs.lix-module.nixosModules.default
     inputs.srvos.nixosModules.mixins-nix-experimental
     inputs.srvos.nixosModules.mixins-trusted-nix-caches
   ];
@@ -10,7 +11,6 @@
 
   nix = {
     checkConfig = false;
-    package = pkgs.lix;
     registry = {
       nixpkgs.flake = inputs.nixpkgs;
       self.flake = inputs.self;
@@ -24,10 +24,12 @@
     optimise.automatic = true;
     settings = {
       extra-substituters = [
+        "https://cache.lix.systems"
         "https://nix-community.cachix.org"
         "https://numtide.cachix.org"
       ];
       extra-trusted-public-keys = [
+        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       ];
