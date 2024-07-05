903334b77f13f7043c932586bc851ea7cf8a2b31
nat <nat@nekopon.pl>
2024-07-05 20:53:36 +0200

    secrets: rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   host-blavingad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDA6kIVhyu6xav3zsUP2UFbr4MWOKnn0rYkYqf1eQ8wx";
-  user-nat-blahaj = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGeYYGkVH8pPo1f769OHYn6Vga6wnhftJA6w2IJADzgs";
+  user-nat-blahaj = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDcOe5wgtrprr3PtIq2w8PWveAGuP6y+p4yd0FxU7x8P";
 in
 {
   "xkomhotshot.age".publicKeys = [
