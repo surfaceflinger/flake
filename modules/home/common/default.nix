521d5329672a9d6143ec9936f206070f7123da6f
nat <nat@nekopon.pl>
2024-07-05 20:55:12 +0200

    modules/home/common/nix: rm
    not needed anymore

--- modules/home/common/default.nix
+++ modules/home/common/default.nix
@@ -3,7 +3,6 @@
   imports = [
     ./git.nix
     inputs.nix-index-database.hmModules.nix-index
-    ./nix.nix
   ];
 
   home.stateVersion = "24.05";
--- modules/home/common/nix.nix
+++
@@ -1,5 +0,0 @@
-{ inputs, ... }:
-{
-  nix.registry.nixpkgs.flake = inputs.nixpkgs;
-  home.sessionVariables.NIX_PATH = "nixpkgs=flake:nixpkgs\${NIX_PATH:+:$NIX_PATH}";
-}
