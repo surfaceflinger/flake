1f0681df8b015c63c6e61f4f7f08f4d2f6651f3d
nat <nat@nekopon.pl>
2024-07-28 12:22:36 +0200

    modules/nixos/common/nix: remove unnecessary thingies

--- modules/nixos/common/nix.nix
+++ modules/nixos/common/nix.nix
@@ -21,17 +21,6 @@
       options = "--delete-older-than 7d";
     };
     optimise.automatic = true;
-    settings = {
-      nix-path = "nixpkgs=flake:nixpkgs";
-      extra-substituters = [
-        "https://nix-community.cachix.org"
-        "https://numtide.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      ];
-    };
   };
 
   system.stateVersion = "24.05";
