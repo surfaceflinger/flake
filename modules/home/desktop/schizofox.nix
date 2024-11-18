04d6d4813a0459bc435c4d360898fd62012a5b9f
nat <nat@nekopon.pl>
2024-11-19 00:10:57 +0100

    modules/home/desktop/schizofox: spoof to ff 132

--- modules/home/desktop/schizofox.nix
+++ modules/home/desktop/schizofox.nix
@@ -11,6 +11,8 @@
   programs.schizofox = {
     enable = true;
 
+    security.userAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0";
+
     settings = {
       # schizofox overrides
       "browser.download.useDownloadDir" = true;
