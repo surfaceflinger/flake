c1b41597b68b40b93c18456059948cafecb7a499
nat <nat@nekopon.pl>
2024-11-18 01:42:05 +0100

    modules/nixos/common/networking: make resolved more reliable

--- modules/nixos/common/networking.nix
+++ modules/nixos/common/networking.nix
@@ -45,24 +45,19 @@
   # dns
   services.resolved = {
     enable = true;
+    dnssec = "false"; # causes resolves to fail way too often
     llmnr = "false";
     fallbackDns = [
-      "1.0.0.1#cloudflare-dns.com"
-      "1.1.1.1#cloudflare-dns.com"
-      "2606:4700:4700::1001#cloudflare-dns.com"
-      "2606:4700:4700::1111#cloudflare-dns.com"
-      "8.8.4.4#dns.google"
-      "8.8.8.8#dns.google"
-      "2001:4860:4860::8844#dns.google"
-      "2001:4860:4860::8888#dns.google"
-      "9.9.9.11#dns11.quad9.net"
       "149.112.112.11#dns11.quad9.net"
       "2620:fe::11#dns11.quad9.net"
       "2620:fe::fe:11#dns11.quad9.net"
+      "9.9.9.11#dns11.quad9.net"
     ];
     extraConfig = ''
-      MulticastDNS=false
+      Cache=no-negative
       DNSOverTLS=opportunistic
+      MulticastDNS=false
+      StaleRetentionSec=28800
     '';
   };
 
