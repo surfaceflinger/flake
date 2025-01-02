7158eede902325aa2c7d953a450ae9770cd13044
Matei Dibu <contact@mateidibu.dev>
2025-01-02 14:53:43 +0200

    modules/nixos/common: add vm-swappines back

--- modules/nixos/common/memory.nix
+++ modules/nixos/common/memory.nix
@@ -7,4 +7,8 @@
 
   environment.etc."sysctl.d/10-pop-default-settings.conf".source =
     inputs.pop-os-default-settings + /etc/sysctl.d/10-pop-default-settings.conf;
+
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 180;
+  };
 }
