0da6ba4ed9cd95b9fd6dbadaea4a0bb57d4fa463
nat <nat@nekopon.pl>
2025-01-13 08:19:46 +0100

    modules/nixos/desktop/unharden: disable hidepid
    it breaks gnome wayland

--- modules/nixos/desktop/unharden.nix
+++ modules/nixos/desktop/unharden.nix
@@ -1,10 +1,9 @@
-{ lib, ... }:
+{ config, lib, ... }:
 {
   nix-mineral.overrides = {
     desktop = {
       allow-multilib = true;
       allow-unprivileged-userns = true;
-      hideproc-ptraceable = true;
       home-exec = true;
       tmp-exec = true;
       disable-usbguard = true;
@@ -14,5 +13,15 @@
     security = {
       disable-bluetooth-kmodules = lib.mkForce false;
     };
+  };
+
+  boot.specialFileSystems."/proc" = {
+    options = lib.mkForce [
+      "nosuid"
+      "nodev"
+      "noexec"
+      "hidepid=0"
+      "gid=${toString config.users.groups.proc.gid}"
+    ];
   };
 }
