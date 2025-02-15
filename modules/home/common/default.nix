c09062758b614c2b1869e716ca2fa919532fb7cf
nat <nat@nekopon.pl>
2025-02-15 21:26:49 +0100

    modules/home/common: enable dont-track-me; fix build

--- modules/home/common/default.nix
+++ modules/home/common/default.nix
@@ -2,8 +2,14 @@
 {
   imports = [
     ./git.nix
+    inputs.dont-track-me.homeManagerModules.default
     inputs.nix-index-database.hmModules.nix-index
   ];
 
   home.stateVersion = osConfig.system.stateVersion;
+
+  dont-track-me = {
+    enable = true;
+    enableAll = true;
+  };
 }
--- modules/home/common/git.nix
+++ modules/home/common/git.nix
@@ -4,12 +4,12 @@
     enable = true;
     diff-so-fancy.enable = true;
     signing = {
+      format = "ssh";
       key = "~/.ssh/id_ed25519";
       signByDefault = true;
     };
     extraConfig = {
       core.editor = with pkgs; (lib.getExe nano);
-      gpg.format = "ssh";
       http.postBuffer = 524288000;
       init.defaultBranch = "main";
       pull.rebase = true;
