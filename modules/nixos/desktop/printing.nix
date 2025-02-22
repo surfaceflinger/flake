89cf599e7fb051d0b59977d790769d50c01c71a7
nat <nat@nekopon.pl>
2025-02-22 01:19:25 +0100

    modules/nixos/desktop/printing: actually ensure printer

--- modules/nixos/desktop/printing.nix
+++ modules/nixos/desktop/printing.nix
@@ -1,9 +1,15 @@
-{ lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   services.printing = {
     enable = true;
-    webInterface = false;
+    startWhenNeeded = false;
     stateless = true;
+    webInterface = false;
   };
 
   hardware.printers = {
@@ -17,5 +23,28 @@
         PageSize = "A4";
       };
     };
+  };
+
+  systemd.services.ensure-printers = {
+    after = [
+      "network-online.target"
+      "nss-lookup.target"
+    ];
+    wants = [ "network-online.target" ];
+    wantedBy = lib.mkForce [ ];
+    requires = [ "nss-lookup.target" ];
+    startAt = "*-*-* *:05:00";
+
+    script = lib.mkBefore ''
+      if ! ${lib.getExe pkgs.curl} "skogsduva:631" &>/dev/null; then
+        echo "Cannot setup printer. Host is down."
+        exit 0
+      fi
+
+      if ${pkgs.cups}/bin/lpstat -p ${config.hardware.printers.ensureDefaultPrinter} &>/dev/null; then
+        echo "Printer already configured."
+        exit 0
+      fi
+    '';
   };
 }
