3beb2ac0bc27d5d40a999f3781ac96b8905f5e74
nat <nat@nekopon.pl>
2025-03-02 15:34:37 +0100

    secrets: rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,4 @@
 let
-  host-blavingad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDA6kIVhyu6xav3zsUP2UFbr4MWOKnn0rYkYqf1eQ8wx";
   host-skogsduva = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAFC2wynlCNvuMdeXAGp0ce1gds3SXJgy/7cRINFK1bR";
   host-jattelik = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbKC5x6WVnOklmnbu/7jIkKtBBhutIdwmF7tweA6i14";
   user-nat-blahaj = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIACA/8h6byidHxDaO0kMWHdKYYpPwRW1nrVPOZr90YiW";
@@ -9,17 +8,14 @@ in {
     user-nat-blahaj
   ];
   "googlebackup.age".publicKeys = [
-    host-blavingad
     host-jattelik
     user-nat-blahaj
   ];
   "vaultwarden.age".publicKeys = [
-    host-blavingad
     host-jattelik
     user-nat-blahaj
   ];
   "wastebin.age".publicKeys = [
-    host-blavingad
     host-jattelik
     user-nat-blahaj
   ];
