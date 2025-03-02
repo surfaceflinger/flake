cb3b7aacfba3ab477902bb3b931d993cee8d101a
nat <nat@nekopon.pl>
2025-03-02 19:45:05 +0100

    hosts/jattelik/personal/vaultwarden: harden, enable features

--- hosts/jattelik/personal/vaultwarden.nix
+++ hosts/jattelik/personal/vaultwarden.nix
@@ -9,17 +9,17 @@
       SMTP_FROM_NAME = "nat's vaultwarden";
       INVITATION_ORG_NAME = "nat's vaultwarden";
       SIGNUPS_ALLOWED = false;
-      INVITATIONS_ALLOWED = false;
       USER_ATTACHMENT_LIMIT = 1048576;
       ORG_ATTACHMENT_LIMIT = 1048576;
       DOMAIN = "https://vault.nekopon.pl";
       EMERGENCY_ACCESS_ALLOWED = false;
+      SHOW_PASSWORD_HINT = false;
       ROCKET_ADDRESS = "::1";
       ROCKET_PORT = 9500;
       PUSH_ENABLED = true;
       SMTP_HOST = "smtp.postmarkapp.com";
       SMTP_FROM = "root@vault.nekopon.pl";
-      EXPERIMENTAL_CLIENT_FEATURE_FLAGS = "autofill-overlay,autofill-v2,browser-fileless-import,fido2-vault-credentials";
+      EXPERIMENTAL_CLIENT_FEATURE_FLAGS = "autofill-overlay,autofill-v2,browser-fileless-import,extension-refresh,fido2-vault-credentials,inline-menu-positioning-improvements,ssh-key-vault-item,ssh-agent";
     };
   };
 
@@ -28,8 +28,8 @@
     extraConfig = ''
       header {
         Strict-Transport-Security "max-age=31536000;"
-        X-XSS-Protection "1; mode=block"
-        X-Frame-Options "DENY"
+        X-XSS-Protection "0"
+        X-Frame-Options "SAMEORIGIN"
         X-Robots-Tag "noindex, nofollow"
         X-Content-Type-Options "nosniff"
         -Server
