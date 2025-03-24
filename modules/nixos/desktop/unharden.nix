3f21123bb83caf17f23c96c727a9a50d1c63a457
nat <nat@nekopon.pl>
2025-03-24 22:43:37 +0100

    modules/nixos/desktop/unharden: skip restrict home permission

--- modules/nixos/desktop/unharden.nix
+++ modules/nixos/desktop/unharden.nix
@@ -8,9 +8,10 @@
     desktop = {
       allow-multilib = true;
       allow-unprivileged-userns = true;
-      home-exec = true;
-      tmp-exec = true;
       disable-usbguard = true;
+      home-exec = true;
+      skip-restrict-home-permission = true;
+      tmp-exec = true;
       yama-relaxed = true;
     };
     performance.no-pti = true;
