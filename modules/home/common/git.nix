890059a0db5ceef571e44c82400d56fa205cdac1
nat <nat@nekopon.pl>
2025-04-09 21:36:46 +0200

    modules/home/common/git: modern config

--- modules/home/common/git.nix
+++ modules/home/common/git.nix
@@ -2,17 +2,32 @@
 {
   programs.git = {
     enable = true;
-    diff-so-fancy.enable = true;
     signing = {
       format = "ssh";
       key = "~/.ssh/id_ed25519";
       signByDefault = true;
     };
     extraConfig = {
-      core.editor = with pkgs; (lib.getExe nano);
+      am.threeWay = true;
+      branch.sort = "-committerdate";
+      column.ui = "auto";
+      commit.verbose = true;
+      help.autoCorrect = "prompt";
       http.postBuffer = 524288000;
       init.defaultBranch = "main";
+      merge.conflictstyle = "zdiff3";
       pull.rebase = true;
+      tag.sort = "version:refname";
+      core = {
+        editor = with pkgs; (lib.getExe nano);
+        whitespace = "trailing-space,space-before-tab,tab-in-indent";
+      };
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
     };
   };
 }
