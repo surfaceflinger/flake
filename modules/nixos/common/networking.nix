22391886699abcc7241fa0ac5ac45588604f99e4
nat <nat@nekopon.pl>
2025-04-29 01:55:41 +0200

    modules/nixos/common/networking: reenable few things

--- modules/nixos/common/networking.nix
+++ modules/nixos/common/networking.nix
@@ -81,9 +81,18 @@
     "net.ipv4.tcp_fin_timeout" = 10;
     "net.ipv4.tcp_slow_start_after_idle" = 0;
 
+    # keepalive
     "net.ipv4.tcp_keepalive_time" = 60;
     "net.ipv4.tcp_keepalive_intvl" = 10;
     "net.ipv4.tcp_keepalive_probes" = 6;
+
+    # reenable few things disabled by hardening
+    "net.ipv4.icmp_echo_ignore_all" = 0;
+    "net.ipv4.tcp_dsack" = 1;
+    "net.ipv4.tcp_sack" = 1;
+    "net.ipv6.icmp.echo_ignore_all" = 0;
+    "net.ipv6.icmp.echo_ignore_anycast" = 0;
+    "net.ipv6.icmp.echo_ignore_multicast" = 0;
 
     # mtu probing
     "net.ipv4.tcp_mtu_probing" = 1;
